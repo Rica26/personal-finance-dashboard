71a2937f4904025c6589d4c300d917e7d5843402
Ricardo Almeida <114597635+Rica26@users.noreply.github.com>
2025-09-16 15:59:18 +0100

    Setup Next.js frontend and add initial APIViews for transaction filters and basic yearly/monthly summaries.

--- backend/transactions/urls.py
+++ backend/transactions/urls.py
@@ -1,8 +1,26 @@
 from rest_framework.routers import DefaultRouter
-from .views import TransactionViewSet, CategoryViewSet
+from django.urls import path, include
+from .views import ExpenseAPIView, IncomeAPIView, TransactionViewSet, CategoryViewSet, IncomeYearlyAPIView, IncomeYearlyTotalAPIView, IncomeYearlyCategoryAPIView, IncomeYearlyMonthlyCategoryAPIView, IncomeMonthlyAPIView, IncomeMonthlyCategoryAPIView, ExpenseYearlyAPIView, ExpenseYearlyTotalAPIView, ExpenseYearlyCategoryAPIView, ExpenseYearlyMonthlyCategoryAPIView, ExpenseMonthlyAPIView, ExpenseMonthlyCategoryAPIView
 
 router = DefaultRouter()
 router.register(r'transactions', TransactionViewSet, basename='transaction')
 router.register(r'categories', CategoryViewSet, basename='category')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('transactions/income/', IncomeAPIView.as_view(), name='income'),
+    path('transactions/income/yearly/<int:year>/', IncomeYearlyAPIView.as_view(), name='income-yearly'),
+    path('transactions/income/yearly/<int:year>/total/', IncomeYearlyTotalAPIView.as_view(), name='income-yearly-total'),
+    path('transactions/income/yearly/<int:year>/category/', IncomeYearlyCategoryAPIView.as_view(), name='income-yearly-category'),
+    path('transactions/income/yearly/<int:year>/monthly-category/', IncomeYearlyMonthlyCategoryAPIView.as_view(), name='income-yearly-monthly-category'),
+    path('transactions/income/monthly/<int:year>/<int:month>/', IncomeMonthlyAPIView.as_view(), name='income-monthly'),
+    path('transactions/income/monthly/<int:year>/<int:month>/category/', IncomeMonthlyCategoryAPIView.as_view(), name='income-monthly-category'),
+    path('transactions/expense/', ExpenseAPIView.as_view(), name='expense'),
+    path('transactions/expense/yearly/<int:year>/', ExpenseYearlyAPIView.as_view(), name='expense-yearly'),
+    path('transactions/expense/yearly/<int:year>/total/', ExpenseYearlyTotalAPIView.as_view(), name='expense-yearly-total'),
+    path('transactions/expense/yearly/<int:year>/category/', ExpenseYearlyCategoryAPIView.as_view(), name='expense-yearly-category'),
+    path('transactions/expense/yearly/<int:year>/monthly-category/', ExpenseYearlyMonthlyCategoryAPIView.as_view(), name='expense-yearly-monthly-category'),
+    path('transactions/expense/monthly/<int:year>/<int:month>/', ExpenseMonthlyAPIView.as_view(), name='expense-monthly'),
+    path('transactions/expense/monthly/<int:year>/<int:month>/category/', ExpenseMonthlyCategoryAPIView.as_view(), name='expense-monthly-category'),
+]
+
+urlpatterns += router.urls
--- backend/transactions/views.py
+++ backend/transactions/views.py
@@ -1,8 +1,13 @@
+from django.db.models import Sum
+from django.db.models.functions import ExtractMonth
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Transaction, Category
 from .serializers import TransactionSerializer, CategorySerializer
 # Create your views here.
 
+#ViewSets
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.select_related('category')
     serializer_class = TransactionSerializer
@@ -11,127 +16,74 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-#utils
 
-#serializers
+#APIViews
 
-#endpoints
+class IncomeAPIView(APIView):
+    def get(self, request):
+        qs = Transaction.objects.filter(category__type='income').annotate(month=ExtractMonth('date')).values('month').annotate(total=Sum('amount')).order_by('month')
+        return Response(qs)
+class IncomeYearlyAPIView(APIView):
+    def get(self,request, year):
+        qs = Transaction.objects.filter(date__year=year, category__type='income').values(month=ExtractMonth('date')).annotate(total=Sum('amount')).order_by('month')
+        return Response(qs)
 
-
-# def filter_by_period(qs, period, year=None, month=None):
-#     if period == 'year' and year:
-#         qs = qs.filter(date__year=year)
-#     elif period == 'month' and year and month:
-#         qs = qs.filter(date__year=year, date__month=month)
-#     return qs
-
-# def filter_by_category(qs, type=None, category_id=None, name=None):
-#     if type and name:
-#         if name not in Category.NAME_CHOICES[type]:
-#             raise ValueError(f"Categoria '{name}' não pertence ao tipo '{type}'")
-        
-
-#     if type:
-#         qs = qs.filter(category__type=type)
-#     if category_id:
-#         qs = qs.filter(category__id=category_id)
-#     if name:
-#         qs = qs.filter(category__name=name)
-#     return qs
-
-# #endpoints
-
-# @api_view(['GET'])
-# def income_yearly(request, year):
-#     transactions = Transaction.objects.select_related('category').all()
-#     transactions = filter_by_period(transactions, 'year', year=year)
-#     transactions = filter_by_category(transactions, type='income')
-#     total = transactions.aggregate(total=Sum('amount'))['total'] or 0
-#     return Response({'year': year, 'income_total': total})
-
-# @api_view(['GET'])
-# def income_monthly(request, year, month):
-#     transactions = Transaction.objects.select_related('category').all()
-#     transactions = filter_by_period(transactions, 'month', year=year, month=month)
-#     transactions = filter_by_category(transactions, type='income')
-#     total = transactions.aggregate(total=Sum('amount'))['total'] or 0
-#     return Response({'year': year, 'month': month, 'income_total': total})
-
-# @api_view(['GET'])
-# def expense_yearly(request, year):
-#     transactions = Transaction.objects.select_related('category').all()
-#     transactions = filter_by_period(transactions, 'year', year=year)
-#     transactions = filter_by_category(transactions, type='expense')
-#     total = transactions.aggregate(total=Sum('amount'))['total'] or 0
-#     return Response({'year': year, 'expense_total': total})
-
-# @api_view(['GET'])
-# def expense_monthly(request, year, month):
-#     transactions = Transaction.objects.select_related('category').all()
-#     transactions = filter_by_period(transactions, 'month', year=year, month=month)
-#     transactions = filter_by_category(transactions, type='expense')
-#     total = transactions.aggregate(total=Sum('amount'))['total'] or 0
-#     return Response({'year': year, 'month': month, 'expense_total': total})
-
-
-#@api_view(['GET'])
-# def summary(request):
-#     all_transactions = Transaction.objects.select_related('category').all()
-#     def total_by_type(trans_type):
-#         return all_transactions.filter(category__type=trans_type).aggregate(total=Sum('amount'))['total'] or 0
-#     income_total = total_by_type('income')
-#     expense_total = total_by_type('expense')
-#     balance = income_total - expense_total
-
-#     def total_by_category(trans_type):
-#         qs = all_transactions.filter(category__type=trans_type).values('category__name').annotate(total=Sum('amount'))
-#         return {item['category__name']: item['total'] for item in qs}
+class IncomeYearlyTotalAPIView(APIView):
+    def get(self, request, year):
+        total = Transaction.objects.filter(date__year=year, category__type='income').aggregate(total=Sum('amount'))['total'] or 0
+        return Response({'year': year, 'total': total})
     
-#     income_by_category = total_by_category('income')
-#     expense_by_category = total_by_category('expense')
+class IncomeYearlyCategoryAPIView(APIView):
+    def get(self, request, year):
+        qs = Transaction.objects.filter(date__year=year, category__type='income').values('category__name').annotate(total=Sum('amount')).order_by('category__name')
+        return Response(qs)
+    
+class IncomeYearlyMonthlyCategoryAPIView(APIView):
+    def get(self, request, year):
+        qs = Transaction.objects.filter(date__year=year, category__type='income').values('category__name', month=ExtractMonth('date')).annotate(total=Sum('amount')).order_by('month', 'category__name')
+        return Response(qs)
 
-#     def monthly_totals(trans_type):
-#         qs = (all_transactions.filter(category__type = trans_type).annotate(month=TruncMonth('date')).values('month').annotate(total=Sum('amount')).order_by('month'))
-#         return [{'month': item['month'].strftime('%Y-%m'), 'total': item['total']} for item in qs]
+class IncomeMonthlyAPIView(APIView):
+    def get (self, request, year, month):
+        total = Transaction.objects.filter(date__year=year, date__month=month, category__type='income').aggregate(total=Sum('amount'))['total'] or 0
+        return Response({'year': year, 'month': month, 'total': total})
+    
+class IncomeMonthlyCategoryAPIView(APIView):
+    def get(self, request, year, month):
+        qs = Transaction.objects.filter(date__year=year, date__month=month, category__type='income').values('category__name').annotate(total=Sum('amount')).order_by('category__name')
+        return Response(qs)
 
-#     income_monthly = monthly_totals('income')
-#     expense_monthly = monthly_totals('expense')
+class ExpenseAPIView(APIView):
+    def get(self, request):
+        qs = Transaction.objects.filter(category__type='expense').annotate(month=ExtractMonth('date')).values('month').annotate(total=Sum('amount')).order_by('month')
+        return Response(qs)
 
-#     balance_monthly = {}
-#     for item in income_monthly:
-#         month = item['month']
-#         balance_monthly[month] = balance_monthly.get(month, 0) + item['total']
-#     for item in expense_monthly:
-#         month = item['month']
-#         balance_monthly[month] = balance_monthly.get(month, 0) - item['total']
-#     balance_monthly = [{'month': month, 'total': total} for month, total in sorted(balance_monthly.items())]
+class ExpenseYearlyAPIView(APIView):
+    def get(self, request, year):
+        qs = Transaction.objects.filter(date__year=year, category__type='expense').values(month=ExtractMonth('date')).annotate(total=Sum('amount')).order_by('month')
+        return Response(qs)
 
-#     def monthly_category(trans_type):
-#         qs = (all_transactions.filter(category__type=trans_type).annotate(month=TruncMonth('date')).values('month', 'category__name').annotate(total=Sum('amount')).order_by('month'))
-#         return [{'month': item['month'].strftime('%Y-%m'), 'category': item['category__name'], 'total': item['total']} for item in qs]
-#     income_category_monthly = monthly_category('income')
-#     expense_category_monthly = monthly_category('expense')
+class ExpenseYearlyTotalAPIView(APIView):
+    def get(self, request, year):
+        total = Transaction.objects.filter(date__year=year, category__type='expense').aggregate(total=Sum('amount'))['total'] or 0
+        return Response({'year': year, 'total': total})
 
-#     balance_category_monthly = {}
-#     for item in income_category_monthly:
-#         key = (item['month'], item['category'])
-#         balance_category_monthly[key] = balance_category_monthly.get(key, 0) + item['total']
-#     for item in expense_category_monthly:
-#         key = (item['month'], item['category'])
-#         balance_category_monthly[key] = balance_category_monthly.get(key, 0) - item['total']
-#     balance_category_monthly = [{'month': month, 'category': category, 'total': total} for (month, category), total in sorted(balance_category_monthly.items())]
+class ExpenseYearlyCategoryAPIView(APIView):
+    def get(self, request, year):
+        qs = Transaction.objects.filter(date__year=year, category__type='expense').values('category__name').annotate(total=Sum('amount')).order_by('category__name')
+        return Response(qs)
 
-#     data = {
-#     'income_total': income_total,
-#     'expense_total': expense_total,
-#     'balance': balance,
-#     'income_by_category': income_by_category,
-#     'expense_by_category': expense_by_category,
-#     'income_monthly': income_monthly,
-#     'expense_monthly': expense_monthly,
-#     'balance_monthly': balance_monthly,
-#     'income_category_monthly': income_category_monthly,
-#     'expense_category_monthly': expense_category_monthly,
-#     'balance_category_monthly': balance_category_monthly,
-#     }
-    #return Response(data)
+class ExpenseYearlyMonthlyCategoryAPIView(APIView):
+    def get(self, request, year):
+        qs = Transaction.objects.filter(date__year=year, category__type='expense').values('category__name', month=ExtractMonth('date')).annotate(total=Sum('amount')).order_by('month', 'category__name')
+        return Response(qs)
+
+class ExpenseMonthlyAPIView(APIView):
+    def get (self, request, year, month):
+        total = Transaction.objects.filter(date__year=year, date__month=month, category__type='expense').aggregate(total=Sum('amount'))['total'] or 0
+        return Response({'year': year, 'month': month, 'total': total})
+
+class ExpenseMonthlyCategoryAPIView(APIView):
+    def get(self, request, year, month):
+        qs = Transaction.objects.filter(date__year=year, date__month=month, category__type='expense').values('category__name').annotate(total=Sum('amount')).order_by('category__name')
+        return Response(qs)
